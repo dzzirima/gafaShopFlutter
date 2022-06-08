2eb1a4fada6108e936f22fbcd09cbc24b28755c6
blackbox <dzzirima@gmail.com>
2022-06-09 00:14:37 +0200

    feat :creating a  user class using an extention

---
+++ lib/features/auth/services/auth_service.dart
@@ -0,0 +1,16 @@
+//connection to backend is handled in here
+class AuthService {
+  //signup user
+  //we might need the build context so we will see the error , ie snackbar
+
+  void signUpUser({
+    required String email,
+    required String name,
+    required String password,
+  }) async {
+    try {} catch (e) {
+      //create a  model for the user
+
+    }
+  }
+}
---
+++ lib/model/user.dart
@@ -0,0 +1,11 @@
+// this file will hold the the model of the user
+
+// DU Data Class Generator
+class User {
+  final String id;
+  final String name;
+  final String password;
+  final String address;
+  final String type;
+  final String token;
+}
